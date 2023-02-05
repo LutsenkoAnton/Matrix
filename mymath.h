1c30efa53bec9a72357e19f4388903f9008a8b07
antonlutsenko <lutsenko.anton@icloud.com>
2023-02-05 14:23:10 +0300

    Added concepts to mymath.h

--- mymath.h
+++ mymath.h
@@ -1,19 +1,27 @@
 #pragma once
 
 #include <cstddef>
+#include <iostream>
+#include "myconcepts.h"
 
-template<typename T>
+template<RingWithOne T>
 T abs(const T& a) {
-    return (a >= static_cast<T>(0) ? a : -a);
+    return (a >= T::ZERO ? a : -a);
 }
 
-template<typename T>
+template<RingWithOne T>
 T fastpow(const T& a, size_t n) {
-    if (n == 0) return T(1);
+    if (n == 0) return T::ONE;
     if (n % 2 == 0) {
         return fastpow(a * a, n / 2);
     }
     return fastpow(a, n - 1) * a;
 }
 
-
+template<EuclideanRing T>
+T gcd(const T& a, const T& b) {
+    if (b == T::ZERO) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
