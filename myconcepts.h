3822c772e3c8185d8ae053c0a1bc777aa599836a
antonlutsenko <lutsenko.anton@icloud.com>
2023-02-05 14:18:27 +0300

    Added concepts

---
+++ myconcepts.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//These concepts are made so that i can better understand what methods do i need
+
+template<typename T>
+concept RingWithOne = requires (T a, T b) { // I can't check basic acsioms, but it is convenient to require these operators
+    a + b;
+    a - b;
+    T::ZERO;
+    T::ONE;
+    -a;
+    a * b;
+};
+
+template<typename T>
+concept EuclideanRing = RingWithOne<T> && requires(T a, T b){
+    a % b;
+    a / b;
+};
+
+template<typename T>
+concept Field = RingWithOne<T> && !EuclideanRing<T> && requires(T a, T b) {
+    a / b;
+};
